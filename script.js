eafde152fef2bd91b740490e0a17d279d5bad7ad
Somayeh Khanbabaei <somayeh.khanbabaei21@gmail.com>
2024-04-23 12:53:23 +0200

    add hamburger menu for responsive header

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth Scroll
+
+document.querySelector('.navbar-nav').addEventListener('click', function(e){
+    e.preventDefault();
+        if(e.target.classList.contains('nav-link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth'
+        })
+    }
+})
+
+
+// Modal Window
